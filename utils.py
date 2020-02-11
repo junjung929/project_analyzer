d854cb5d70d8bba52572cb67fbf6faa943039d82
JunJung <juny929@gmail.com>
2020-02-11 18:00:14 +0200

    Added errer handling

--- utils.py
+++ utils.py
@@ -11,28 +11,28 @@ def get(url, api, auth=None):
     try:
         response = requests.request("GET", endpoint, auth=auth)
         return json.loads(response.text)
-    except Exception as e:
-        print(e)
-        return -1
+    except requests.exceptions.RequestException as e:
+        raise requests.exceptions.RequestException(e)
+
 
 def post(url, api, params={}, auth=None):
     endpoint = url + api
     try:
         response = requests.request("POST", endpoint, auth=auth, params=params)
         return json.loads(response.text)
-    except Exception as e:
-        print(e)
-        return -1
+    except requests.exceptions.RequestException as e:
+        raise requests.exceptions.RequestException(e)
+
 
 def getBasicAuth(username, password):
     return requests.auth.HTTPBasicAuth(username, password)
 
+
 def getConfig(target):
-    if ENV == "development":
-        config_path = "config.test.json"
-    else:
-        config_path = "config.json"
-    
-    with open(config_path) as config_data:
-        config = json.load(config_data)
-    return config.get(target)
+    config_path = "config.test.json" if ENV == "development" else "config.json"
+    try:
+        with open(config_path) as config_data:
+            config = json.load(config_data)
+        return config.get(target)
+    except Exception as e:
+        raise Exception(e)
